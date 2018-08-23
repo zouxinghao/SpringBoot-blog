19a94b768d2796c490a26c6aa9bc29cb85d9e70e
xzou <527309305@qq.com>
2018-08-23 23:21:25 +0800

    :whale: Add mySQL database with docker support

---
+++ src/main/java/com/zxh/myBlog/App.java
@@ -0,0 +1,13 @@
+package com.zxh.myBlog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
