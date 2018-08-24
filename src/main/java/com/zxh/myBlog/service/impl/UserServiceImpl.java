aa7095747c74a0f41bb2ddc737e5063f7cda6871
xzou <527309305@qq.com>
2018-08-24 12:55:26 +0800

    :construction: add user function

--- src/main/java/com/zxh/myBlog/service/impl/UserServiceImpl.java
+++ src/main/java/com/zxh/myBlog/service/impl/UserServiceImpl.java
@@ -1,5 +1,34 @@
 package com.zxh.myBlog.service.impl;
 
-public class UserServiceImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.zxh.myBlog.model.Vo.UserVo;
+import com.zxh.myBlog.service.IUserService;
+
+public class UserServiceImpl implements IUserService{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+	
+	
+	public Integer insertUser(UserVo userVo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserVo queryById(Integer uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserVo login(Integer username, Integer password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateById(UserVo userVo) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
