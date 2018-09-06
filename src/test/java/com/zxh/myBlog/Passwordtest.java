3ad9ae44532641e64af9ac820e3ea4d8d5c0f292
xzou <527309305@qq.com>
2018-09-06 22:39:45 +0800

    :white_check_mark: integrate test code

--- src/test/java/com/zxh/myBlog/Passwordtest.java
+++ src/test/java/com/zxh/myBlog/Passwordtest.java
@@ -1,30 +1,15 @@
 package com.zxh.myBlog;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.zxh.myBlog.model.Vo.UserVo;
 import com.zxh.myBlog.utils.TaleUtils;
 
-
-@Disabled
-@RunWith(JUnitPlatform.class)
-@DisplayName("Testing Login System ... ")
-public class Passwordtest{
-	private static final Logger LOGGER = LoggerFactory.getLogger(Passwordtest.class);
-	@Test
-	@DisplayName("Testing password function ...")
-	public void testPassword () {
-		LOGGER.info("@Test:testPassword");
+public class Passwordtest {
+	public static void mian(String args[]) {
 		UserVo user = new UserVo();
 		user.setUsername("admin");
 		user.setPassword("J9lew2irojE23");
-		String encodePwd = TaleUtils.MD5encode(user.getUsername() + user.getPassword());
-		//assertEquals()
+		String encode = TaleUtils.MD5encode(user.getUsername() + user.getPassword());
+		System.out.println(encode);
 	}
 }
+
